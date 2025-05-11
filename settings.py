1b0f2486ddaf4863eb2578e75687d77675443bc2
dubasher <iladubinin403@gmail.com>
2025-05-11 16:32:21 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,10 @@
+valid_email = 'igor_chunaev@gmail.com'
+valid_password = 'igorCh.78.'
+
+not_valid_email = 'igor@mail.ru'
+not_valid_password = 'password_123'
+
+vacant_email = ''
+vacant_password = ''
+
+empty_auth_key = {'key': ''}
